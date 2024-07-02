69e4bedc0bdffc4746ef410963826394cbd2a113
Joshua_Musyoki <musyokijoxhua@gmail.com>
2024-07-02 21:13:09 +0300

    Data types

--- Datatypes.dart
+++ Datatypes.dart
@@ -1,9 +1,37 @@
 void main(){
   String greetings = "Good Evening";
   String name = "Joshua";
-
+  int apples = 5;
   print("${greetings}, ${name}!");
+
+  double someDouble = 5.567;
+  print(someDouble.round());
+
+  print("I have $apples apples");
+  // print("I have + apples.toString() + apples");
+  print("I have ${apples.toString()} apples");
+
+  // String util methods
+  print('Never odd or even'.contains('odd'));
+  print('Never odd or even'.startsWith('Never'));
+  print('hello devs'.toUpperCase());
+  print(''.isEmpty);
  
+
+//  Booleans
+bool isLocationEnabled = false;
+
+if (!isLocationEnabled) {
+  checkCurrentLocation();
+} else{
+  
+}
+    // ignore: dead_code
+    // return "Location Services Enabled";
+
+}
+
+void checkCurrentLocation() {
 }
 
 class Items {
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 6-Month-Rule---Android-Development
+## Introduction to Dart
+Dart has 9 Data types
+1. String
+2. Numbers - Integers(Whole numbers) and Double(floated)
+3. Booleans
+4. Lists
+5. Maps
+6. Sets
+7. Runes
+8. Symbols
+9. The value 'null'
+
+
