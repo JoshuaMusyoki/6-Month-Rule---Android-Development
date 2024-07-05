84ad81a9793e9e92f0f35d2f8e0c5f3a4e0aafda
Joshua_Musyoki <musyokijoxhua@gmail.com>
2024-07-05 21:40:37 +0300

    Writing Stateful Widgets - Counter App

--- Flutter-Mobile/chat_app/lib/main.dart
+++ Flutter-Mobile/chat_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/chart_page.dart';
+import 'package:chat_app/counter_stateful_demo.dart';
 import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,7 @@ class ChatApp extends StatelessWidget {
           backgroundColor: Colors.yellow,
         ),
       ),
-      home: ChatPage()
+      home: CounterStateful()
     );
   }
 
