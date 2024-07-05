fb7405ded4938aa7633fc864f302df333ba489a6
Joshua_Musyoki <musyokijoxhua@gmail.com>
2024-07-05 20:40:07 +0300

    Creating your own widgets

--- Flutter-Mobile/chat_app/lib/chart_page.dart
+++ Flutter-Mobile/chat_app/lib/chart_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_buble.dart';
+import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -25,113 +27,12 @@ class ChatPage extends StatelessWidget {
           Expanded(
             child: ListView(
               children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    padding: EdgeInsets.all(24),
-                    child:
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text("Hi. This is your Message.",
-                          style: TextStyle(fontSize: 24, color: Colors.white),
-                        ),
-                        Image.network("https://picsum.photos/250?image=9", height: 200)
-                      ],
-                    ),
-                    margin: EdgeInsets.all(50),
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          topRight: Radius.circular(12),
-                          bottomLeft: Radius.circular(12),
-                          bottomRight: Radius.circular(12)
-                      ),
-                    ),
-            
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    padding: EdgeInsets.all(24),
-                    child:
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text("Hi. This is your Message.",
-                          style: TextStyle(fontSize: 24, color: Colors.white),
-                        ),
-                        Image.network("https://picsum.photos/250?image=9", height: 200)
-                      ],
-                    ),
-                    margin: EdgeInsets.all(50),
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          topRight: Radius.circular(12),
-                          bottomLeft: Radius.circular(12),
-                          bottomRight: Radius.circular(12)
-                      ),
-                    ),
-            
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    padding: EdgeInsets.all(24),
-                    child:
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text("Hi. This is your Message.",
-                          style: TextStyle(fontSize: 24, color: Colors.white),
-                        ),
-                        Image.network("https://picsum.photos/250?image=9", height: 200)
-                      ],
-                    ),
-                    margin: EdgeInsets.all(50),
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(12),
-                          topRight: Radius.circular(12),
-                          bottomLeft: Radius.circular(12),
-                          bottomRight: Radius.circular(12)
-                      ),
-                    ),
-            
-                  ),
-                ),
+                ChatBubble(alignment: Alignment.centerLeft, message: "Hello! This is Joshua"),
+                ChatBubble(alignment: Alignment.centerRight, message: "Good Evening!!")
               ],
             ),
           ),
-
-        Container(
-          height: 100,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                onPressed: (){},
-                icon: Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ),
-              ),
-              IconButton(
-                  onPressed: (){},
-                  icon: Icon(Icons.send))
-            ],
-          ),
-          decoration: BoxDecoration(
-              color: Colors.black45,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20))
-          ),
-        )
+          ChatInput(),
         ],
       ),
     );
